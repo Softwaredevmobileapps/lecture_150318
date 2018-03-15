d1f127293e44b517954cb7f1579396da862e3884
Martin Kenirons <martin.kenirons@gmit.ie>
2018-03-15 16:07:41 +0000

    Lecture Upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Flashlight } from '@ionic-native/flashlight';
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+isOn:boolean = false;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private flashlight: Flashlight) {
+  }
+  openAbout(){
+    this.navCtrl.push("AboutPage");
+  }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+  lightOn(){
+this.flashlight.switchOn();
+this.updateStatus();
+  }
+  lightOff(){
+    this.flashlight.switchOff();
+    this.updateStatus();
+  }
+  updateStatus(){
+    this.isOn = this.flashlight.isSwitchedOn();
   }
 
 }
